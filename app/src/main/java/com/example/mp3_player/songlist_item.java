46e6a184d3db364092ed327d200a28baa8e30f3d
Michał Krajewski <nerazeem6000@gmail.com>
2019-06-02 20:47:13 +0200

    Pliki projektu

---
+++ app/src/main/java/com/example/mp3_player/songlist_item.java
@@ -0,0 +1,13 @@
+package com.example.mp3_player;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class songlist_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_songlist_item);
+    }
+}
